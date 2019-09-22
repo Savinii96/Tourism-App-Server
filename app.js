09e12990eb0c4c75f35743e917d38c0cf0ac36f8
Savini <saviniamarasinghe@gmail.com>
2019-09-22 18:11:38 +0530

    ready for deployment

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const dotenv = require("dotenv");
+dotenv.config();
 
 mongoose.connect(config.database);
 
@@ -15,7 +17,7 @@ mongoose.connection.on('connected', () => {
 const app = express();
 
 // port number
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const users = require('./routes/users');
 const attraction = require('./routes/newAtt');
@@ -24,7 +26,18 @@ const reviews = require('./routes/reviews');
 
 
 
-app.use(cors());
+// app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(cors({
+    "origin": ["*","http://localhost:4200"],
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
